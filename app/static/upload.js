af47acfe6608adeffcfcac500da8faf3c0a7e46b
wuxuef2 <wuxuef2@qq.com>
2015-01-12 22:01:24 +0800

    finish upload image and adjust points

--- app/static/upload.js
+++ app/static/upload.js
@@ -1,12 +1,39 @@
 $(function() {
+	$("#showArea").hide();
 	var url = location.href + "/upload";
-	var imgPath = location.href + "/static/img/";
+	var imgPath = location.href + "static/img/";
 	$('#file').attr("data-url", url);
 	$('#file').on('success.tools.upload', function(json) {
-		alert(json.path);
-		$("#uploadArea").empty();
-		$("#uploadArea")
+		$("#showArea").show();
+		$("#uploadArea").hide();
+		$("#uploadFace").attr("src", imgPath + json.path);
 		
-		//console.log(this, json);
+		var html = [];
+		var obj = $("#imgArea");
+		var offsetTop = obj.offset().top;
+		var offsetLeft = obj.offset().left;
+		var css = {
+			width: json.width,
+			height: json.height
+		};
+		obj.css(css);
+		
+		for (var i = 0; i < json.pointSize; i++) {
+			var tmpLeft = offsetLeft + json.pointX[i];
+			var tmpTop = offsetTop + json.pointY[i];			
+			html.push('<a class="blackPoint" index="' + i + '" href="#" style="left:' + tmpLeft + 'px; top:' + tmpTop + 'px;"></a>');
+		}
+		html = html.join("");		
+		obj.append(html);
+		
+		$(".blackPoint").draggable({
+			containment: "parent",
+		  	cursor: "crosshair",
+		  	stop: function(event, ui) {
+		  		var index = parseInt(ui.helper.attr("index"));
+		  		json.pointX[index] = ui.offset.left;
+		  		json.pointY[index] = ui.offset.top;
+		  	}
+		});
 	});
 });
--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ import os
 from flask import Flask, request, redirect, url_for
 from flask import json, jsonify
 from werkzeug import secure_filename
+import random
+from PIL import Image
 
 # index view function suppressed for brevity
 
@@ -22,7 +24,6 @@ def login():
 
 ALLOWED_EXTENSIONS = set(['bmp', 'BMP', 'tif', 'TIF', 'tiff', 'TIFF', 'png', 'PNG', 'jpg', 'JPG', 'jpeg', 'JPEG', 'gif', 'GIF'])
 
-
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -31,10 +32,30 @@ def allowed_file(filename):
 def index():
     if request.method == 'POST':
         file = request.files['file']
-        print file
+                
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return jsonify(path = filename)
+            img = Image.open(app.config['UPLOAD_FOLDER'] + '/' + filename)
+            
+            width = img.size[0]
+            height = img.size[1]
+            
+            pointX = []
+            for i in range(0, 68):
+                pointX.append(random.randint(0, width))
+                
+            pointY = []
+            for i in range(0, 68):
+                pointY.append(random.randint(0, height))
+            
+            
+            
+            return jsonify(path = filename, 
+                           width = img.size[0], 
+                           height = img.size[1],
+                           pointX = pointX,
+                           pointY = pointY,
+                           pointSize = 68)
     return render_template('upload.html')
     
