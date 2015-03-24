45e5c76f08653d14effe476c3e3d6f52445034cd
wuxuef2 <wuxuef2@qq.com>
2015-03-24 19:31:17 +0800

    version 0.1

--- app/static/upload.js
+++ app/static/upload.js
@@ -2,81 +2,109 @@ $(function() {
 	var agingOption = {};
 	var offsetTop = 0;
 	var offsetLeft = 0;
+	var agingCounter = 0;
 	
 	$("#showArea").hide();
-	var url = location.href + "/upload";
-	var imgPath = location.href + "static/img/";
+	var IP = "http://127.0.0.1:5000/";
+	var url = IP + "upload";
+	var imgPath = IP + "static/img/";
 	$('#file').attr("data-url", url);
 	$('#file').on('success.tools.upload', function(json) {
-		$("#showArea").show();
-		$("#uploadArea").hide();
-		$("#uploadFace").attr("src", imgPath + json.path);
-		
-		agingOption.pointX = json.pointX;
-		agingOption.pointY = json.pointY;
-		
-		var obj = $("#imgArea");
-		offsetTop = obj.offset().top;
-		offsetLeft = obj.offset().left;
-		
-		var html = [];		
-		var css = {
-			width: json.width,
-			height: json.height
-		};
-		obj.css(css);
-		
-		for (var i = 0; i < json.pointSize; i++) {
-			var tmpLeft = offsetLeft + json.pointX[i];
-			var tmpTop = offsetTop + json.pointY[i];			
-			html.push('<a class="blackPoint" y="' + json.pointY[i] + '" x="' + json.pointX[i] + '" index="' + i 
-				+ '" href="#" style="left:' + tmpLeft + 'px; top:' + tmpTop + 'px;"></a>');
+		if (json.code == 0) {
+			json = json.data;
+			$("#showArea").show();
+			$("#uploadArea").hide();
+			$("#uploadFace").attr("src", imgPath + json.path);
+			
+			agingOption.pointX = json.pointX;
+			agingOption.pointY = json.pointY;
+			
+			var obj = $("#imgArea");
+			offsetTop = obj.offset().top - 5;
+			offsetLeft = obj.offset().left - 5;
+			
+			var html = [];		
+			var css = {
+				width: json.width,
+				height: json.height
+			};
+			obj.css(css);
+			
+			for (var i = 0; i < json.pointSize; i++) {
+				var tmpLeft = offsetLeft + json.pointX[i];
+				var tmpTop = offsetTop + json.pointY[i];			
+				html.push('<a class="blackPoint" y="' + json.pointY[i] + '" x="' + json.pointX[i] + '" index="' + i 
+					+ '" href="#" style="left:' + tmpLeft + 'px; top:' + tmpTop + 'px;"></a>');
+			}
+			html = html.join("");		
+			obj.append(html);
+			$("#newFace").hide();
+			$(".blackPoint").draggable({
+				containment: "parent",
+			  	cursor: "crosshair",
+			  	stop: function(event, ui) {
+			  		var index = parseInt(ui.helper.attr("index"));
+			  		agingOption.pointX[index] = ui.position.left - offsetLeft;
+			  		agingOption.pointY[index] = ui.position.top - offsetTop;
+			  	}
+			});
+			
+			$("#curAge").val(Math.floor(json.curAge));
+		} else {
+			alert(json.mess);
 		}
-		html = html.join("");		
-		obj.append(html);
-		$("#newFace").hide();
-		$(".blackPoint").draggable({
-			containment: "parent",
-		  	cursor: "crosshair",
-		  	stop: function(event, ui) {
-		  		var index = parseInt(ui.helper.attr("index"));
-		  		agingOption.pointX[index] = ui.position.left - offsetLeft;
-		  		agingOption.pointY[index] = ui.position.top - offsetTop;
-		  	}
-		});
 	});
 	
 	$("#Aging").on("click", function(){
 		agingOption.curAging = $("#curAge").val();
-		agingOption.forecastAge = $("#forecastAge").val();
-		var pointX = [];
-		var pointY = [];
-		$(".blackPoint").each(function(){
-			var i = $(this).attr("index");
-			pointX[i] = $(this).position().left - offsetLeft;
-			pointY[i] = $(this).position().top - offsetTop;
-		});
+		agingOption.forecastAge = $("#forecastAge").val();		
 		
-		var points = [];
-		var index = 0;
-		for (var i = 0; i < pointX.length; i++) {
-			points[index] = pointX[i];
-			index++;
-			points[index] = pointY[i];
-			index++;
-		}
-		
-		var str = $("#uploadFace").attr("src");
-		index = str.lastIndexOf("/") + 1;
-		agingOption.image = str.substr(index);
-		agingOption.points = points.join(" ");		
-		$.post("/age", agingOption, function(data) {
-			if (data.code === 0) {
-				$("#newFace").attr("src", imgPath + data.newImage);
-				$("#newFace").show();
+		var cAge = parseInt(agingOption.curAging);
+		var pAge = parseInt(agingOption.forecastAge);
+		// if (cAge === pAge) {
+			// alert("Current Age equal to prective Age");			
+		// } else if (cAge > 15 || pAge > 15) {
+			// alert("Age large than 15.\n Application couldn't support so far.");			
+		// } else {		
+			var pointX = [];
+			var pointY = [];
+			$(".blackPoint").each(function(){
+				var i = $(this).attr("index");
+				pointX[i] = $(this).position().left - offsetLeft;
+				pointY[i] = $(this).position().top - offsetTop;
+			});
+			
+			var points = [];
+			var index = 0;
+			for (var i = 0; i < pointX.length; i++) {
+				points[index] = pointX[i];
+				index++;
+				points[index] = pointY[i];
+				index++;
 			}
-		});
-		
+			
+			var str = $("#uploadFace").attr("src");
+			index = str.lastIndexOf("/") + 1;
+			agingOption.image = str.substr(index);
+			agingOption.points = points.join(" ");	
+			agingOption.times = agingCounter;
+			agingCounter++;	
+			$.post("/age", agingOption, function(data) {
+				if (data.code === 0) {
+					var html = [];
+					html.push('<div class="agingFace">');
+						html.push('<img id="newFace' + agingCounter + '" src=" ' + imgPath + data.newImage + ' " alt="face" />');
+						html.push('<br />');
+						html.push('<span>Current Age:' + data.curAge + '</span>');
+						html.push('<br />');
+						html.push('<span>predictive Age:' + data.forecastAge + '</span>');
+					html.push('</div>');
+					html = html.join("");
+					
+					$("#agingFaceShow").append(html);
+				}
+			});			
+		// }
 		return false;
 	});
 });
--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ import random
 from PIL import Image
 import ctypes
 import time
+import string
 
 # index view function suppressed for brevity
 
@@ -18,48 +19,77 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+def package(code, mess, data):
+    return jsonify(code = code,
+                   mess = mess,
+                   data = data)
+ERROR_MESSAGE = ["Normal",                      #0
+                 "Cannot Read Uploaded Image",  #1
+                 "Not Find Face",               #2
+                 "System Busy",                 #3
+                 "Age Input Error",             #4
+                 "Shapes != Images",            #5
+                 "Age error!",                  #6
+                 "The image channels must be 3, and the depth must be 8!"]#7    
+           
+def upload():
+    file = request.files['file']
+                
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        imagePath = app.config['UPLOAD_FOLDER'] + '/' + filename
+        ticks = repr(time.time())
+        
+        
+        os.renames(imagePath, app.config['UPLOAD_FOLDER'] + '/' + ticks + filename)
+        filename = ticks + filename
+        imagePath = app.config['UPLOAD_FOLDER'] + '/' + filename
+        
+        try:
+            img = Image.open(imagePath)
+        except:
+            return package(-1, "App couldn't understand the upload file.", {})
+        
+        width = img.size[0]
+        height = img.size[1]
+        pdll = ctypes.CDLL("./wuxuef.so")
+        pdll.getShape.restype = ctypes.POINTER(ctypes.c_double * 137)
+        points = []
+        
+        points = pdll.getShape(imagePath, "1").contents
+
+
+        pointX = []     
+        pointY = []
+        
+        curAge = int(points[68 * 2])
+        if curAge < 0:
+            return package(-1, ERROR_MESSAGE[-curAge], {})
+                     
+        for i in range(0, 68):
+            pointX.append(points[i * 2])
+            pointY.append(points[i * 2 + 1])          
+
+        return package(0, "", {'path' : filename, 
+                        'width' : img.size[0], 
+                        'height' : img.size[1],
+                        'pointX' : pointX,
+                        'pointY' : pointY,
+                        'curAge' : points[68 * 2],
+                        'pointSize' : 68})
+    return package(-1, "App couldn't understand the upload file.", {})
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        file = request.files['file']
-        #curAge = request.files['curAge']
-                
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            imagePath = app.config['UPLOAD_FOLDER'] + '/' + filename
-            ticks = repr(time.time())
-            
-            
-            os.renames(imagePath, app.config['UPLOAD_FOLDER'] + '/' + ticks + filename)
-            filename = ticks + filename
-            imagePath = app.config['UPLOAD_FOLDER'] + '/' + filename
-            img = Image.open(imagePath)
-            
-            width = img.size[0]
-            height = img.size[1]
-            pdll = ctypes.CDLL("./wuxuef.so")
-            pdll.getShape.restype = ctypes.POINTER(ctypes.c_double * 136)
-            points = []
-            
-            points = pdll.getShape(imagePath, "1").contents
-            #print (len(points))
-            #print points[0:136]
-            pointX = []     
-            pointY = []
-                         
-            for i in range(0, 68):
-                pointX.append(points[i * 2])
-                pointY.append(points[i * 2 + 1])          
-
-            return jsonify(path = filename, 
-                            width = img.size[0], 
-                            height = img.size[1],
-                            pointX = pointX,
-                            pointY = pointY,
-                            pointSize = 68)
+        return upload()
             
     return render_template('upload.html')
+
+@app.route('/Help', methods=['GET'])
+def support():          
+    return render_template('Help.html')
     
 
 @app.route('/age', methods=['POST'])
@@ -68,16 +98,22 @@ def age():
     curAge = request.form['curAging']
     forecastAge = request.form['forecastAge']
     points = request.form['points']
+    counter = request.form['times']
      
     pdll = ctypes.CDLL("./wuxuef.so")
     pdll.fit.argtypes = [ctypes.c_char_p, 
                          ctypes.c_char_p, 
                          ctypes.c_char_p, 
                          ctypes.c_char_p, 
+                         ctypes.c_int,
                          ctypes.c_int]
     
     image = app.config['UPLOAD_FOLDER'] + '/' + imagePath
-    newImage = 'result_' + imagePath
-    
-    code = pdll.fit(image, curAge, forecastAge, points, 136, newImage)
-    return jsonify(code = code, newImage = newImage)
+    newImage = 'result_' + counter + imagePath
+    print(newImage)
+    code = pdll.fit(image, curAge, forecastAge, points, 136, string.atoi(counter))
+    return jsonify(code = code, newImage = newImage, curAge = curAge, forecastAge = forecastAge)
+
+@app.route('/upload', methods=['POST'])
+def uploadImage():
+    return upload()
